9ab4514e2f0a63308dc6ef01316808164272a56f
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-28 15:32:19 +0200

    poprawa zadania 2

--- src/zadanie2/Add.java
+++ src/zadanie2/Add.java
@@ -1,17 +1,13 @@
 package zadanie2;
 
-import java.util.Scanner;
-
 public class Add {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number=scanner.nextInt();
 
-        int sum=0;
-        while(number !=0){
-            sum+=number%10;
-            number/=10;
+        public int sumNumber(int number){
+            int sum = 0;
+            while (number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
         }
-        System.out.println(sum);
     }
-}
---
+++ src/zadanie2/AddTest.java
@@ -0,0 +1,7 @@
+package zadanie2;public class AddTest {
+    public static void main(String[] args) {
+        int number= 12345;
+        Add add= new Add();
+        System.out.println(add.sumNumber(number));
+    }
+}
